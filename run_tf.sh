afc3599660b6216725bfceb4a4cf4ee77f21146f
Darius Tehrani <darius.tehrani@microsoft.com>
2019-05-13 13:08:44 +0200

    path issues

--- run_tf.sh
+++ run_tf.sh
@@ -248,15 +248,15 @@ VAR_FILE_PATH=$(get_abs_filename "${i}")
 ensure_subription_context
 
 .log 6 "[==== 00 Ensure Terraform State Backend  ====]"
-ensure_terraform_backend "00-tf-backend" "${VAR_FILE_PATH}"
+ensure_terraform_backend "./00-tf-backend" "${VAR_FILE_PATH}"
 
 .log 6 "[==== 01 AKS ====]"
-run_terraform ${v} ${e} "01-aks" "${VAR_FILE_PATH}"
+run_terraform ${v} ${e} "./01-aks" "${VAR_FILE_PATH}"
 
 .log 6 "[==== 03 Post Deploy ====]"
-run_terraform ${v} ${e} "03-aks-post-deploy" "${VAR_FILE_PATH}"
+run_terraform ${v} ${e} "./03-aks-post-deploy" "${VAR_FILE_PATH}"
 
 .log 6 "[==== 04 Post Deploy Ingress ====]"
-run_terraform ${v} ${e} "04-aks-post-deploy-ingress" "${VAR_FILE_PATH}"
+run_terraform ${v} ${e} "./04-aks-post-deploy-ingress" "${VAR_FILE_PATH}"
 
 .log 6 "[==== Done. ====]"
