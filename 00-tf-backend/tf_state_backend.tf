3f313ba7a442c16bc3fbac9bda2c684fc485cffe
Darius Tehrani <darius.tehrani@microsoft.com>
2019-05-13 14:34:14 +0200

    backend

--- 00-tf-backend/tf_state_backend.tf
+++ 00-tf-backend/tf_state_backend.tf
@@ -9,3 +9,24 @@ EOF
   filename = "${path.module}/../backend.tfvars"
 }
 
+
+output "resource_group_name" {
+  value = "${var.resource_group_name}"
+}
+
+output "storage_account_name" {
+  value = "${var.storage_account_name}"
+}
+
+output "container_name" {
+  value = "${var.storage_container_name}"
+}
+
+output "access_key" {
+  value     = "${var.storage_account_primary_access_key}"
+  sensitive = true
+}
+
+output "backend_config_params" {
+  value     = "-backend-config 'resource_group_name=${var.resource_group_name}' -backend-config 'storage_account_name=${var.storage_account_name}' -backend-config 'container_name=${var.storage_container_name}' -backend-config 'access_key=${var.storage_account_primary_access_key}'"
+  sensitive = true
