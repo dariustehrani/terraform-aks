bfa2e7d5c4c74ee4560c1a3630aa67f69496799b
Darius Tehrani <darius.tehrani@microsoft.com>
2019-05-13 15:05:20 +0200

    dummy

--- 00-tf-backend/tf_state_backend.tf
+++ 00-tf-backend/tf_state_backend.tf
@@ -1,13 +1,9 @@
-#resource "local_file" "backend_config_aks" {
-#  content = <<EOF
-# resource_group_name  = "${var.resource_group_name}"
-# storage_account_name = "${var.storage_account_name}"
-# container_name       = "${var.storage_container_name}"
-# access_key           = "${var.storage_account_primary_access_key}"
-# EOF
-#
-#  filename = "${path.module}/../backend.tfvars"
-# }
+resource "local_file" "backend_config_aks" {
+  content = <<EOF
+  dummy
+  EOF
+  filename = "${path.module}/../dummy.txt"
+ }
 
 
 output "resource_group_name" {
